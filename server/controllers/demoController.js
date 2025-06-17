170088c60e49fe155c0c4aa1fe65169523b51ef0
Apolline Setan <161021816+ApollineSetan@users.noreply.github.com>
2025-06-17 12:09:28 +0200

    corrects error req.file.buffer

--- server/controllers/demoController.js
+++ server/controllers/demoController.js
@@ -30,8 +30,8 @@ const demoController = {
             return res.status(400).json({ error: "Image file is required." });
             }
 
-            const image_url = req.file.location; // URL publique S3
-            const audio_url = req.body.audio_url || null; // ou rajoute support audio ici plus tard
+            const image_url = req.file.location;
+            const audio_url = req.body.audio_url || null;
 
             const newDemo = {
             title,
@@ -47,58 +47,30 @@ const demoController = {
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
-},
-
-//     async createDemo(req, res) {
-//     try {
-//       const { title, description, duration, section_id, image_url } = req.body;
-
-//       if (!req.file || !req.file.location) {
-//         return res.status(400).json({ error: "Audio file is required." });
-//       }
-
-//       const audio_url = req.file.location;
-
-//       const newDemo = {
-//         title,
-//         description,
-//         image_url,
-//         duration,
-//         section_id,
-//         audio_url,
-//       };
-
-//       const demo = await demoManager.create(newDemo);
-
-//       res.status(201).json(demo);
-//     } catch (error) {
-//       res.status(500).json({ error: error.message });
-//     }
-//   },
+    },
 
     async updateDemo(req, res) {
-  try {
-    // Construire l'objet demo à mettre à jour
-    const demoData = {
-      title: req.body.title,
-      description: req.body.description,
-      image_url: req.file ? req.file.buffer : req.body.image_url, // ou req.file.path si tu stockes sur disque
-      duration: req.body.duration,
-      section_id: req.body.section_id,
-      audio_url: req.body.audio_url,
-    };
+        try {
+            const demoData = {
+            title: req.body.title,
+            description: req.body.description,
+            image_url: req.file ? req.file.location : req.body.image_url,
+            duration: req.body.duration,
+            section_id: req.body.section_id,
+            audio_url: req.body.audio_url,
+            };
 
-    const demo = await demoManager.update(req.params.id, demoData);
+            const demo = await demoManager.update(req.params.id, demoData);
 
-    if (!demo) {
-      return res.status(404).json({ message: "Demo not found" });
-    }
+            if (!demo) {
+            return res.status(404).json({ message: "Demo not found" });
+            }
 
-    res.json(demo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-},
+            res.json(demo);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
 
     async deleteDemo(req, res) {
