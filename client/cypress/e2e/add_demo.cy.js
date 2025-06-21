45509490bbe616f333cf1a0068d1948cd332335b
Apolline Setan <161021816+ApollineSetan@users.noreply.github.com>
2025-06-21 17:22:08 +0200

    test add demo

--- client/cypress/e2e/add_demo.cy.js
+++ client/cypress/e2e/add_demo.cy.js
@@ -2,35 +2,26 @@
 
 describe('Ajout d\'une démo audio', () => {
   beforeEach(() => {
-    // Adapter l'URL selon ta route d'ajout
     cy.visit('/add-demo');
   });
 
+  // Title, description and section
   it('remplit le formulaire et ajoute une démo', () => {
-    // Remplir le titre
-    cy.get('#titleInput').type('Ma super démo');
-
-    // Remplir la description
+    cy.get('#titleInput').type('Titre de ma démo');
     cy.get('#descriptionInput').type('Voici la description de ma démo.');
-
-    // Choisir une section si besoin, sinon commenter cette partie
     cy.get('#sectionSelect').then($select => {
       if ($select.find('option').length > 1) {
-        // Sélectionne la 2ème option (première option est désactivée)
         cy.get('#sectionSelect').select($select.find('option').eq(1).val());
       }
     });
 
-    // Upload fichier audio (depuis fixtures)
+    // Upload audio and image (from fixtures)
     cy.get('#audioFile').attachFile('testaudio.mp3');
-
-    // Upload image (depuis fixtures)
     cy.get('#imageFile').attachFile('testimage.png');
 
-    // Cliquer sur Valider
     cy.get('button[type=submit]').click();
 
-    // Vérifier redirection ou comportement attendu après soumission
+    // Test redirection
     cy.url().should('eq', Cypress.config().baseUrl + '/');
   });
 });
